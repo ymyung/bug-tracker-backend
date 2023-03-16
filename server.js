baca3d5f66aa0a1ad1c00223a8bea2a7a08efc6c
Yo Myung <yosepmyung@gmail.com>
2023-03-16 03:33:59 -0600

    cors

--- server.js
+++ server.js
@@ -11,11 +11,7 @@ const userRoute = require('./routes/userRoute')
 const app = express()
 
 // cors
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://bug-tracker-frontend-44ie.onrender.com');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use(cors())
 
 // middleware
 app.use(express.json())
